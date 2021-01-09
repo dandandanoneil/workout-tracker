c4fe3ef4f7c97b0587729f7e9caf34b209e6c8aa
Dan ONeil <dhoneil@gmail.com>
2021-01-08 22:41:42 -0500

    re-deploy with new MongoDB Atlas connection

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
